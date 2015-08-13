28a756e3292a514216b2e149ee6c5b885e2ea59d
Ilya Muravev <ilya.yarn@gmail.com>
2015-08-13 14:55:06 +0400

    Fix sources to correspond with language level

--- src/main/java/com/ilyamur/osgi/helloworld/impl/Activator.java
+++ src/main/java/com/ilyamur/osgi/helloworld/impl/Activator.java
@@ -8,15 +8,13 @@ import java.util.Hashtable;
 
 public class Activator implements BundleActivator {
 
-    @Override
     public void start(BundleContext bundleContext) throws Exception {
-        Hashtable<String, String> props = new Hashtable<>();
+        Hashtable<String, String> props = new Hashtable<String, String>();
         props.put("Language", "English");
         bundleContext.registerService(OsgiHelloworldService.class.getName(), new OsgiHelloworldServiceImpl(), props);
         System.out.println("osgi-helloworld start");
     }
 
-    @Override
     public void stop(BundleContext bundleContext) throws Exception {
         System.out.println("osgi-helloworld stop");
     }
